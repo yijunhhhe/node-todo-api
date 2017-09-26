134b1dc20447187625b25f6d438d5d139288e9be
Yijun <yijunhhhe@gmail.com>
2017-09-26 16:34:39 -0400

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,23 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if(err){
+        console.log('unable to connect to mongodb server');
+    } 
+    console.log('connected to mongodb server');
+    
+    db.collection('Todos').find({
+        _id: new ObjectID("59caa94440a3d54c986ad932")
+    }).toArray().then((docs) => {
+        console.log('todo');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('unable to fetch todos', err);
+    })
+    
+    //db.close();
+});
+
